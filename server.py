c913b1da3c8e2c9473a499abdff0034a48542863
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-11-08 14:29:53 +0300

    add getting image from camera

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, Response
 from settings import templates, CAMERA_SETTINGS, PHOTO_NAME
 from services import save_file, save_photo
 from services.cv import prepare_image, fetch_faces_from_image, get_name_by_faces
@@ -35,6 +35,18 @@ def get_face_by_image_from_camera():
     if faces is None:
         return {"result": "error"}
     return {"result": get_name_by_faces(faces)}
+
+
+@app.get("/get-image")
+def get_image_from_camera():
+    return Response(
+        get_photo_from_camera(
+            CAMERA_SETTINGS["camera_ip"],
+            CAMERA_SETTINGS["username"],
+            CAMERA_SETTINGS["password"],
+        ),
+        media_type="image/jpg",
+    )
 
 
 @app.post("/get-mask")
